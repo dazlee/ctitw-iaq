90f23c151ab68c52104f88ec7c20e5f50220fa8e
Daz <daz.lee1987@gmail.com>
2016-04-29 23:33:06 +0800

    add fetch js to get data via ajax

--- public/client/dashboard.js
+++ public/client/dashboard.js
@@ -1,6 +1,6 @@
 define(function () {
     return {
-        initialize: function () {
+        initialize: function (deviceDate) {
             $('#historychart').highcharts({
                 chart: {
                     spacingBottom: 50,
@@ -82,29 +82,10 @@ define(function () {
                     floating: true,
                     backgroundColor: (Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF'
                 },
-                // tooltip: {
-                //     formatter: function() {
-                //         // var datetimeStr = Highcharts.dateFormat('%Y-%m-%d %H:%M:%S', this.x);
-                //         // return datetimeStr;
-                //         return 'The value for <b>' + this.x + '</b> is <b>' + this.y + '</b>, in series '+ this.series.name;
-                //     },
-                // },
                 series: [{
                     name: '二氧化碳',
                     yAxis: 0,
-                    data: [
-                        [1167692400000, 61.05],
-                        [1167778800000, 58.32],
-                        [1167865200000, 57.35],
-                        [1167951600000, 56.31],
-                        [1168210800000, 55.55],
-                        [1168297200000, 55.64],
-                        [1168383600000, 54.02],
-                        [1168470000000, 51.88],
-                        [1168556400000, 52.99],
-                        [1168815600000, 52.99],
-                        [1168902000000, 51.21],
-                    ],
+                    data: deviceDate.co2,
                     tooltip: {
                         valueDecimals: 2,
                         valueSuffix: ' ppm'
@@ -114,19 +95,7 @@ define(function () {
                 {
                     name: '溫度',
                     yAxis: 1,
-                    data: [
-                        [1167692400000, 36.2],
-                        [1167778800000, 33.2],
-                        [1167865200000, 34.2],
-                        [1167951600000, 35.2],
-                        [1168210800000, 36.2],
-                        [1168297200000, 32.2],
-                        [1168383600000, 26.2],
-                        [1168470000000, 25.2],
-                        [1168556400000, 24.2],
-                        [1168815600000, 23.2],
-                        [1168902000000, 22.2],
-                    ],
+                    data: deviceDate.temperature,
                     tooltip: {
                         valueDecimals: 2,
                         valueSuffix: ' °C'
@@ -137,19 +106,7 @@ define(function () {
                 {
                     name: '濕度',
                     yAxis: 2,
-                    data: [
-                        [1167692400000, 60.2],
-                        [1167778800000, 56.2],
-                        [1167865200000, 76.2],
-                        [1167951600000, 88.2],
-                        [1168210800000, 78.2],
-                        [1168297200000, 76.2],
-                        [1168383600000, 86.2],
-                        [1168470000000, 94.2],
-                        [1168556400000, 56.2],
-                        [1168815600000, 78.2],
-                        [1168902000000, 67.2],
-                    ],
+                    data: deviceDate.moisture,
                     tooltip: {
                         valueDecimals: 2,
                         valueSuffix: ' %'
