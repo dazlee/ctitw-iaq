c201e64f56f511d8bca3ef76ab9986fdd34adf01
Daz <daz.lee1987@gmail.com>
2016-04-30 11:57:18 +0800

    reformat indentation

--- app/Http/Controllers/DeviceHistoryController.php
+++ app/Http/Controllers/DeviceHistoryController.php
@@ -14,52 +14,52 @@ class DeviceHistoryController extends Controller
     private $formDataKey = 'file';
     private $limit = 30;
     private $rules = [
-	'device_id' => 'required',
-        'co2' => 'required|numeric',
-        'temp' => 'required|numeric',
-        'rh' => 'required|numeric',
-        'created_at' => 'required|date_format:Y-m-d h:i:s',
-	'updated_at' => 'date_format:Y-m-d h:i:s',
+        'device_id'     => 'required',
+        'co2'           => 'required|numeric',
+        'temp'          => 'required|numeric',
+        'rh'            => 'required|numeric',
+        'created_at'    => 'required|date_format:Y-m-d h:i:s',
+        'updated_at'    => 'date_format:Y-m-d h:i:s',
     ];
-    
+
     private function validator(array $data) {
-	return Validator::make($data, $this->rules);
+        return Validator::make($data, $this->rules);
     }
 
     public function upload(Request $request) {
-	if (!$request->hasFile($this->formDataKey)) {
-	    return response()->json(['err' => 'invalid form-data key'], 406);
-	}
+        if (!$request->hasFile($this->formDataKey)) {
+            return response()->json(['err' => 'invalid form-data key'], 406);
+        }
 
-	$file = $request->file($this->formDataKey);
-	$content = file_get_contents($file);
-	$rows = DeviceHistory::parseContent($content);
-	
-	if (empty($rows)) {
-	    return response()->json(['err' => 'The format of file is uncorrect'], 406);
-	} 
+        $file = $request->file($this->formDataKey);
+        $content = file_get_contents($file);
+        $rows = DeviceHistory::parseContent($content);
 
-	DeviceHistory::insert($rows);
+        if (empty($rows)) {
+            return response()->json(['err' => 'The format of file is uncorrect'], 406);
+        }
+
+        DeviceHistory::insert($rows);
         return response()->json(['msg' => $rows], 201);
     }
 
-    
+
     public function index() {
-	return DeviceHistory::orderBy('created_at', 'asc')->paginate($this->limit*16);
+        return DeviceHistory::orderBy('created_at', 'asc')->paginate($this->limit*16);
     }
-    /*	
+    /*
     public function store(Request $request) {
-	$validator = $this->validator($request->all());
+    $validator = $this->validator($request->all());
 
-	if ($validator->fails()) {
-	    return response()->json($validator->messages(), 401);
-	}
+    if ($validator->fails()) {
+        return response()->json($validator->messages(), 401);
+    }
 
-	return DeviceHistory::create($request->all());
+    return DeviceHistory::create($request->all());
     }
     */
 
     public function show($deivceId) {
-	return DeviceHistory::where('device_id', $deivceId)->orderBy('created_at', 'asc')->paginate($this->limit);
+        return DeviceHistory::where('device_id', $deivceId)->orderBy('created_at', 'asc')->paginate($this->limit);
     }
 }
