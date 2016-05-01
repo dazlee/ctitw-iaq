94fc9cdcb4fa65da1c39e18149e2feb357c95203
Daz <daz.lee1987@gmail.com>
2016-05-01 23:24:20 +0800

    1. add moment js library for time managing 2. can filter device data (right now week only)

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -3,6 +3,7 @@ require.config({
         "client": "../client",
         "lib": "../client/lib",
         "underscore": "../client/lib/underscore/underscore.min",
+        "moment": "../client/lib/moment/moment.min",
         "fetch-utils": "../client/lib/fetch-utils",
         "device-utils": "../client/lib/device-utils",
         "utils": "../client/lib/utils",
