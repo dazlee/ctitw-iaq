9b3bdfc9aacfc8b07ee0f65413c6d1d1bfe026db
Daz <daz.lee1987@gmail.com>
2016-05-16 13:12:16 +0800

    add composer to layout template for default departments data

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,8 +7,42 @@ use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesResources;
+use View;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, AuthorizesResources, DispatchesJobs, ValidatesRequests;
+
+    public function __construct() {
+        // [TODO] should load departments data from DB
+        View::composer('layouts.app', function ($view){
+            $departments = [
+                [
+                    'name'  => '部門1',
+                    'device_id' => 1,
+                ],
+                [
+                    'name'  => '部門2',
+                    'device_id' => 2,
+                ],
+                [
+                    'name'  => '部門3',
+                    'device_id' => 3,
+                ],
+                [
+                    'name'  => '部門4',
+                    'device_id' => 4,
+                ],
+                [
+                    'name'  => '部門5',
+                    'device_id' => 5,
+                ],
+                [
+                    'name'  => '部門6',
+                    'device_id' => 6,
+                ],
+            ];
+            $view->with('departments', $departments);
+        });
+    }
 }
