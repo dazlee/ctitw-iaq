b480779a96f73ee2254e9427f449f949cd71bbdf
Daz <daz.lee1987@gmail.com>
2016-05-16 13:49:50 +0800

    use service provider to register composer, use composer to inject departments data into layouts.app tempalte

---
+++ app/Providers/ComposerServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class ComposerServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        view()->composer(
+            'layouts.app', 'App\ViewComposers\LayoutComposer'
+        );
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
