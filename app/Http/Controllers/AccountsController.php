aeed9978a09793c3693f1ded1134551d42df8253
HsinHeng <erick93054@gmail.com>
2016-05-17 16:11:08 +0800

    remove unneed code

--- app/Http/Controllers/AccountsController.php
+++ app/Http/Controllers/AccountsController.php
@@ -116,34 +116,6 @@ class AccountsController extends Controller
         return Redirect::back();
     }
 
-    public function createDepartment(Request $request) {
-        $this->validate($request, [
-            'name' => 'required|max:255',
-            'email' => 'required|email|max:255|unique:users',
-            'password' => 'required|min:6',
-            'device_id' => 'required|unique:departments,device_id'
-        ]);
-
-        DB::transaction(function($request) use ($request) {
-            $user = User::create([
-                'name' => $request->get('name'),
-                'email' => $request->get('email'),
-                'password' => bcrypt($request->get('password'))
-            ]);
-
-            $role = Role::where('name', '=', 'department')->first();
-            $user->attachRole($role);
-
-            $department = new Department();       
-            $department->client_id = Auth::id();
-            $department->device_id = $request->get('device_id');
-            $department->phone = $request->get('phone');
-            $user->department()->save($department);
-        });
-
-        return Redirect::back();
-    }
-
     public function device () {
         return view('accounts', array(
             "name"      => "儀器",
