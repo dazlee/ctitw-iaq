80a69f3d622443bda61631d87fc62167010869aa
HsinHeng <erick93054@gmail.com>
2016-05-30 15:43:56 +0800

    add deviceAccount Query String

--- app/Builders/BaseBuilder.php
+++ app/Builders/BaseBuilder.php
@@ -19,9 +19,14 @@ class BaseBuilder {
     protected function createQueryOfDevice($request, $deviceId) {
         $fromDate = $request->query('fromDate');
         $toDate = $request->query('toDate');
+        $deviceAccount = $request->query('deviceAccount');
 
         if (isset($fromDate) && isset($toDate)) {
             return DeviceHistory::ofDevice($deviceId)->betweenDates($fromDate, $toDate)->sortRecord('asc');
+        }
+
+        if (isset($deviceAccount)) {
+             return DeviceHistory::like($deviceAccount)->ofDays(6)->sortRecord('asc');
         }
 
         return DeviceHistory::ofDevice($deviceId)->ofDays(6)->sortRecord('asc');
