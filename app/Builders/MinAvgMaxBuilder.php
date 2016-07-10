1cf03be6f082a7ae7d471655fb990d42131ab1a8
Bryan <daz.lee1987@gmail.com>
2016-07-10 00:37:17 -0400

    MinAvgMaxBuilder can detect the devices of client

--- app/Builders/MinAvgMaxBuilder.php
+++ app/Builders/MinAvgMaxBuilder.php
@@ -4,6 +4,7 @@ namespace App\Builders;
 
 use App\Builders\BaseBuilder;
 use App\DeviceHistory;
+use DB;
 
 class MinAvgMaxBuilder extends BaseBuilder{
     private $key = 'min_avg_max';
@@ -16,7 +17,22 @@ class MinAvgMaxBuilder extends BaseBuilder{
 
     public function run() {
         if ($this->request->query($this->key) == 1) {
-            $query = $this->query->selectRaw('device_id,
+
+            $deviceAccount = $this->request->query('deviceAccount');
+            $client = DB::select('select * from clients where device_account = ?', [$deviceAccount]);
+            if (count($client) > 0)  {
+                $client_id = $client[0]->user_id;
+                $devices = DB::select('select * from devices where client_id = ?', [$client_id]);
+                $device_ids = [];
+                foreach ($devices as $device) {
+                    array_push($device_ids, $deviceAccount . '-' . $device->index);
+                }
+                $query = $this->query->whereIn('device_id', $device_ids);
+            } else {
+                $query = $this->query;
+            }
+
+            $query = $query->selectRaw('device_id,
                 MIN(co2) as "co2-min", MIN(temp) as "temp-min", MIN(rh) as "rh-min",
                 AVG(co2) as "co2-avg", AVG(temp) as "temp-avg", AVG(rh) as "rh-avg",
                 MAX(co2) as "co2-max", MAX(temp) as "temp-max", MAX(rh) as "rh-max"
