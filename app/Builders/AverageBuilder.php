a60d0a4d40c9a1d7cff252f1917d0ad1e9d1cedf
Bryan <daz.lee1987@gmail.com>
2016-07-10 00:37:01 -0400

    AverageBuilder can detect the devices of client

--- app/Builders/AverageBuilder.php
+++ app/Builders/AverageBuilder.php
@@ -20,27 +20,28 @@ class AverageBuilder extends BaseBuilder{
             return [$this->key => []];
         }
  
-        if ($this->request->query('timestamp') == 1) {
-            $deviceAccount = $this->request->query('deviceAccount');
-            $client = DB::select('select * from clients where device_account = ?', [$deviceAccount]);
-            if (count($client) > 0)  {
-                $client_id = $client[0]->user_id;
-                $devices = DB::select('select * from devices where client_id = ?', [$client_id]);
-                $device_ids = [];
-                foreach ($devices as $device) {
-                    array_push($device_ids, $deviceAccount . '-' . $device->index);
-                }
-                return [$this->key => $this->query->selectRaw('record_at, AVG(co2) as co2, AVG(temp) as temp, AVG(rh) as rh')->whereIn('device_id', $device_ids)->groupBy('record_at')->get()];
-            } else {
-                return [$this->key => $this->query->selectRaw('record_at, AVG(co2) as co2, AVG(temp) as temp, AVG(rh) as rh')->groupBy('record_at')->get()];
+        $deviceAccount = $this->request->query('deviceAccount');
+        $client = DB::select('select * from clients where device_account = ?', [$deviceAccount]);
+        if (count($client) > 0)  {
+            $client_id = $client[0]->user_id;
+            $devices = DB::select('select * from devices where client_id = ?', [$client_id]);
+            $device_ids = [];
+            foreach ($devices as $device) {
+                array_push($device_ids, $deviceAccount . '-' . $device->index);
             }
+            $query = $this->query->whereIn('device_id', $device_ids);
+        } else {
+            $query = $this->query;
+        }
+
+        if ($this->request->query('timestamp') == 1) {
+                return [$this->key => $query->selectRaw('record_at, AVG(co2) as co2, AVG(temp) as temp, AVG(rh) as rh')->groupBy('record_at')->get()];
         }
 
         if ($this->request->query('device_level') == 1) {
-            return [$this->key => $this->query->selectRaw('device_id, AVG(co2) as co2, AVG(temp) as temp, AVG(rh) as rh')->groupBy('device_id')->get()];
+            return [$this->key => $query->selectRaw('device_id, AVG(co2) as co2, AVG(temp) as temp, AVG(rh) as rh')->groupBy('device_id')->get()];
         }
 
-        $query = $this->query->get();
         return [$this->key => ['co2' => $query->avg('co2'), 'temp' => $query->avg('temp'), 'rh' => $query->avg('rh')]];
     }
 }
